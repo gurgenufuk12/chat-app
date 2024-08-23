5beb6e1a9020d666bad19f64965aec8e115b35b2
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-08-23 11:45:40 +0300

    feat: firebaseConfig updated for db instance

--- src/firebase/firebaseConfig.tsx
+++ src/firebase/firebaseConfig.tsx
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,9 +14,10 @@ const firebaseConfig = {
   storageBucket: "chat-app-b11f2.appspot.com",
   messagingSenderId: "729292214404",
   appId: "1:729292214404:web:f31bf105d40d5a4c5732d1",
-  measurementId: "G-QZPFSRJNP6"
+  measurementId: "G-QZPFSRJNP6",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 export const auth = getAuth(app);
