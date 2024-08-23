ffca01780b470248e92b1fa0bc74226f10dcef59
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-08-23 11:46:03 +0300

    feat: app.tsx route design changed to private route

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,39 @@
 import React from "react";
 import {
   BrowserRouter as Router,
-  Route,
   Routes,
+  Route,
   Navigate,
 } from "react-router-dom";
-import Register from "./components/Register";
+import { AuthProvider, useAuth } from "./contexts/AuthContext";
 import Login from "./components/Login";
-import Home from "./components/Home";
-import { useAuth } from "./contexts/AuthContext";
+import Home from "../src/components/Home";
+import Register from "./components/Register";
+
+const PrivateRoute: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const { currentUser } = useAuth();
+  return currentUser ? <>{children}</> : <Navigate to="/" />;
+};
 
 const App: React.FC = () => {
-  const { currentUser } = useAuth();
   return (
     <Router>
-      <Routes>
-        {/* Only accessible after login */}
-        <Route
-          path="/home"
-          element={currentUser ? <Home /> : <Navigate to="/" replace />}
-        />
-
-        {/* Public routes */}
-        <Route path="/register" element={<Register />} />
-        <Route
-          path="/"
-          element={!currentUser ? <Login /> : <Navigate to="/home" replace />}
-        />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Login />} />
+          <Route
+            path="/home"
+            element={
+              <PrivateRoute>
+                <Home />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 };
