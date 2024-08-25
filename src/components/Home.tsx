fdf8e7ef186c2687163b58ff0792db89008c479b
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-08-25 13:55:22 +0300

    feat: add user to Home Page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import ChatWindow from "../components/ChatWindow";
 import InputBox from "../components/InputBox";
 import Logout from "../components/Logout";
+import { useAuth } from "../contexts/AuthContext";
 
 const HomeContainer = styled.div`
   display: flex;
@@ -14,6 +15,7 @@ const HomeContainer = styled.div`
 const Title = styled.h1`
   font-size: 2rem;
   margin-bottom: 10px;
+  text-align: center;
 `;
 
 const ChatContainer = styled.div`
@@ -22,9 +24,11 @@ const ChatContainer = styled.div`
 `;
 
 const Home: React.FC = () => {
+  const { currentUser } = useAuth();
+
   return (
     <HomeContainer>
-      <Title>Welcome to the Chat Application!</Title>
+      <Title>Welcome {currentUser?.email} to the chat!</Title>
       <ChatContainer>
         <ChatWindow />
         <InputBox />
