403b8fd2985d63df71fca961e30b6181c1a3f5ba
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-08-26 15:15:42 +0300

    feat: handleDeleteChannel function implemented

--- src/components/Channels.tsx
+++ src/components/Channels.tsx
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { collection, addDoc, getDocs } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  deleteDoc,
+  query,
+  where,
+} from "firebase/firestore";
 import { db } from "../firebase/firebaseConfig";
 import { useDispatch } from "react-redux";
 import { setChannel } from "../redux/chatSlice";
 import Close from "@mui/icons-material/Close";
+import Delete from "@mui/icons-material/Delete";
 
 const ChannelsContainer = styled.div`
   display: flex;
@@ -18,6 +27,9 @@ const ChannelsContainer = styled.div`
 `;
 
 const ChannelItem = styled.div<{ selected: boolean }>`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
   padding: 10px;
   cursor: pointer;
   color: white;
@@ -25,7 +37,8 @@ const ChannelItem = styled.div<{ selected: boolean }>`
   border-radius: ${(props) => (props.selected ? "10px" : "0")};
 `;
 
-const AddChannelButton = styled.button`
+const ChannelButton = styled.button`
+  margin-top: 10px;
   padding: 10px;
   background-color: #3dc5b7;
   color: white;
@@ -33,6 +46,13 @@ const AddChannelButton = styled.button`
   border-radius: 4px;
   cursor: pointer;
   align-self: center;
+`;
+const DeletChanneleButton = styled.button`
+  background-color: transparent;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
 `;
 const AddChannelPopup = styled.div`
   position: absolute;
@@ -67,8 +87,25 @@ const CloseButton = styled.button`
   cursor: pointer;
   color: white;
 `;
+const DeleteChannelPopup = styled.div`
+  position: absolute;
+  width: 300px;
+  height: 300px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #353647;
+  padding: 20px;
+  border-radius: 10px;
+  z-index: 100;
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+  justify-content: center;
+`;
 const Channels: React.FC = () => {
   const [channels, setChannels] = useState<string[]>([]);
+  const [deletePopup, setDeletePopup] = useState(false);
   const [channelName, setChannelName] = useState("");
   const [showPopup, setShowPopup] = useState(false);
   const [selectedChannel, setSelectedChannel] = useState<string | null>(null);
@@ -99,7 +136,24 @@ const Channels: React.FC = () => {
       setChannels((prev) => [...prev, channelName]);
     }
   };
+  const handleDeleteChannel = async (channel: string) => {
+    if (!channel) return;
 
+    try {
+      const channelsRef = collection(db, "channels");
+      const q = query(channelsRef, where("name", "==", channel));
+      const querySnapshot = await getDocs(q);
+
+      querySnapshot.forEach(async (docSnapshot) => {
+        const docRef = doc(db, "channels", docSnapshot.id);
+        await deleteDoc(docRef);
+      });
+
+      setChannels((prev) => prev.filter((ch) => ch !== channel));
+    } catch (error) {
+      console.error("Error deleting channel: ", error);
+    }
+  };
   return (
     <ChannelsContainer>
       {channels.map((channel) => (
@@ -109,9 +163,32 @@ const Channels: React.FC = () => {
           onClick={() => handleChannelClick(channel)}
         >
           {channel}
+          <DeletChanneleButton
+            onClick={() => {
+              setDeletePopup(true);
+            }}
+          >
+            <Delete />
+          </DeletChanneleButton>
+          {deletePopup && (
+            <DeleteChannelPopup>
+              <h2>Are you sure you want to delete this channel?</h2>
+              <ChannelButton
+                onClick={() => {
+                  handleDeleteChannel(channel);
+                  setDeletePopup(false);
+                }}
+              >
+                Yes
+              </ChannelButton>
+              <ChannelButton onClick={() => setDeletePopup(false)}>
+                No
+              </ChannelButton>
+            </DeleteChannelPopup>
+          )}
         </ChannelItem>
       ))}
-      <AddChannelButton onClick={openPopup}>Add Channel</AddChannelButton>
+      <ChannelButton onClick={openPopup}>Add Channel</ChannelButton>
       {showPopup && (
         <AddChannelPopup>
           <Input
@@ -120,7 +197,7 @@ const Channels: React.FC = () => {
             placeholder="Enter channel name"
             onChange={(e) => setChannelName(e.target.value)}
           />
-          <AddChannelButton
+          <ChannelButton
             onClick={() => {
               handleAddChannel();
               setShowPopup(false);
@@ -128,7 +205,7 @@ const Channels: React.FC = () => {
             }}
           >
             Add Channel
-          </AddChannelButton>
+          </ChannelButton>
           <CloseButton
             onClick={() => {
               setShowPopup(false);
