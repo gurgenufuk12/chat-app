f0dc6b02a1c5566912a56662d637eca518efb011
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-08-26 14:42:21 +0300

    feat: implement pop-up component for adding channel

--- src/components/Channels.tsx
+++ src/components/Channels.tsx
@@ -4,6 +4,7 @@ import { collection, addDoc, getDocs } from "firebase/firestore";
 import { db } from "../firebase/firebaseConfig";
 import { useDispatch } from "react-redux";
 import { setChannel } from "../redux/chatSlice";
+import Close from "@mui/icons-material/Close";
 
 const ChannelsContainer = styled.div`
   display: flex;
@@ -25,17 +26,51 @@ const ChannelItem = styled.div<{ selected: boolean }>`
 `;
 
 const AddChannelButton = styled.button`
-  margin-top: 20px;
   padding: 10px;
   background-color: #3dc5b7;
   color: white;
   border: none;
   border-radius: 4px;
   cursor: pointer;
+  align-self: center;
 `;
-
+const AddChannelPopup = styled.div`
+  position: absolute;
+  width: 300px;
+  height: 300px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #353647;
+  padding: 20px;
+  border-radius: 10px;
+  z-index: 100;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+const Input = styled.input`
+  padding: 10px;
+  width: 100%;
+  border: none;
+  border-radius: 4px;
+  margin-bottom: 10px;
+  background-color: #414256;
+  color: white;
+`;
+const CloseButton = styled.button`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  color: white;
+`;
 const Channels: React.FC = () => {
   const [channels, setChannels] = useState<string[]>([]);
+  const [channelName, setChannelName] = useState("");
+  const [showPopup, setShowPopup] = useState(false);
   const [selectedChannel, setSelectedChannel] = useState<string | null>(null);
   const dispatch = useDispatch();
 
@@ -51,12 +86,13 @@ const Channels: React.FC = () => {
   }, []);
 
   const handleChannelClick = (channel: string) => {
-    setSelectedChannel(channel); // Update the selected channel
+    setSelectedChannel(channel);
     dispatch(setChannel(channel));
   };
-
+  const openPopup = () => {
+    setShowPopup(true);
+  };
   const handleAddChannel = async () => {
-    const channelName = prompt("Enter channel name:");
     if (channelName) {
       const channelsRef = collection(db, "channels");
       await addDoc(channelsRef, { name: channelName });
@@ -69,15 +105,40 @@ const Channels: React.FC = () => {
       {channels.map((channel) => (
         <ChannelItem
           key={channel}
-          selected={channel === selectedChannel} // Highlight the selected channel
+          selected={channel === selectedChannel}
           onClick={() => handleChannelClick(channel)}
         >
           {channel}
         </ChannelItem>
       ))}
-      <AddChannelButton onClick={handleAddChannel}>
-        Add Channel
-      </AddChannelButton>
+      <AddChannelButton onClick={openPopup}>Add Channel</AddChannelButton>
+      {showPopup && (
+        <AddChannelPopup>
+          <Input
+            type="text"
+            value={channelName}
+            placeholder="Enter channel name"
+            onChange={(e) => setChannelName(e.target.value)}
+          />
+          <AddChannelButton
+            onClick={() => {
+              handleAddChannel();
+              setShowPopup(false);
+              setChannelName("");
+            }}
+          >
+            Add Channel
+          </AddChannelButton>
+          <CloseButton
+            onClick={() => {
+              setShowPopup(false);
+              setChannelName("");
+            }}
+          >
+            <Close />
+          </CloseButton>
+        </AddChannelPopup>
+      )}
     </ChannelsContainer>
   );
 };
