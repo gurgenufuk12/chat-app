aab7690152e43bdf3b56f3680229a01891a0d398
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-08-27 16:59:48 +0300

    feat: .env file added with necessary config values

--- src/firebase/firebaseConfig.tsx
+++ src/firebase/firebaseConfig.tsx
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCaBe599pbnL3Olg6-3Ew6NutRCwkPFM1s",
-  authDomain: "chat-app-b11f2.firebaseapp.com",
-  projectId: "chat-app-b11f2",
-  storageBucket: "chat-app-b11f2.appspot.com",
-  messagingSenderId: "729292214404",
-  appId: "1:729292214404:web:f31bf105d40d5a4c5732d1",
-  measurementId: "G-QZPFSRJNP6",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
